9a2b5341d86f76a2b705ad2c252cfd5cbd3a7c9d
tray <onesimus.githua@student.moringaschool.com>
2023-09-29 00:21:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+            function moveDodgerLeft() {
+                const leftNumbers = dodger.style.left.replace("px", "");
+                   const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+
+  function moveDodgerRight() {
+             let dodger = document.getElementById('dodger');
+                 let currentLeft = parseInt(dodger.style.left) || 0;
+                      dodger.style.left = currentLeft + 10 + 'px';
+  }
